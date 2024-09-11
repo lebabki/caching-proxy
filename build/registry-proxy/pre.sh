4249624ff5bcdabf4769bee21af7101ee2795cd8
Leonid Babkin <64258768+lebabki@users.noreply.github.com>
2024-09-11 15:51:45 +0700

    ...

--- build/registry-proxy/pre.sh
+++ build/registry-proxy/pre.sh
@@ -8,4 +8,7 @@ if [ "$${AUTH_REGISTRIES}" = "::" ]; then
 fi
 
 gomplate -f /etc/gomplate/nginx.conf.gotmpl -o /etc/nginx/nginx.conf
+
+sed -r -i '$ s/^(\$\{NGINX_BIN\})/exec \1/' /entrypoint.sh
+
 exec /entrypoint.sh
